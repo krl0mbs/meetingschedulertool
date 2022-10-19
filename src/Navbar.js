84ccd076fda19bb6fbfe9e601b64fcc558172a4b
Christopher Ferreira <christopher.ferreira9@gmail.com>
2022-10-19 12:17:06 -0400

    navbar stuff

--- src/Navbar.js
+++ src/Navbar.js
@@ -1,5 +1,16 @@
 export default function Navbar() {
     return (<nav>
-        
+        <a href="/" className="mst-home">Meeting Scheduler</a>
+        <ul>
+            <li>
+
+            </li>
+            <li>
+
+            </li>
+            <li>
+                
+            </li>
+        </ul>
     </nav>)
 }
