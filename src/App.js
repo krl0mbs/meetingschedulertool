5bb25442cbc162593e117e5a64477ed9eadcd0f4
Christopher Ferreira <christopher.ferreira9@gmail.com>
2022-10-19 12:11:46 -0400

    navbar stuff

--- src/App.js
+++ src/App.js
@@ -7,34 +7,17 @@ function App() {
     <div className="App">
       {/* <Sidebar pageWrapId={'page-wrap'} outerContainerId={'outer-container'} /> */}
       <header className="App-header"> {/*defines the header, use classname="App-header" to tell which class from App.css will be used on this tag*/}
-        <p>
-          Meeting Scheduler {/*text that will appear in the header*/} &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
-        </p>
-        <a className="Header-item" href="/">
-          Home
-        </a>
-        <p>
-          &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
-        </p>
-        <a className="Header-item" href="/bookroom">
-          Book Room 
-        </a>
-        <p>
-          &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
-        </p>
-        <a className="Header-item" href="/checkbookings">
-          Check Bookings
-        </a>
-        <p>
-          &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
-        </p>
-        <a className="Header-item" href="/managebookings">
-          Manage Bookings
-        </a>
+        <a className="Home-button" href="/">Meeting Scheduler</a>
+        <p>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</p>
+        <a className="Header-item" href="/bookroom">Book Room</a>
+        <p>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</p>
+        <a className="Header-item" href="/checkbookings">Check Bookings</a>
+        <p>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</p>
+        <a className="Header-item" href="/managebookings">Manage Bookings</a>
       </header>
-      <body className="App-body"> {/*defines the header, use classname="App-header" to tell which class from App.css will be used on this tag*/}
-      </body>
+      <body className="App-body"></body>
     </div>
+    
   );
 }
 
