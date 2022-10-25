c035e711bed47ac8b248907c3ae4affcff685f78
Christopher Ferreira <christopher.ferreira9@gmail.com>
2022-10-25 17:31:39 -0400

    latest version of the table

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import { View } from 'react-native-web';
+import React from 'react';
 import './App.css';
 import Table from './table'
+import './table.css'
 
 
 function App() {
@@ -28,93 +30,35 @@ const Availability = () => (
   <Table container flexDirection = 'column' alignItems = ''>
     {/* table below corresponds to the first row which is all headers */}
     <Table flex = {1} container flexDirection = 'row'>
-      <Table width = '100px'>
-        <h4>Hours</h4>
-      </Table>
-      <Table width = '50px'>
-        <h4>0</h4>
-      </Table>
-      <Table width = '50px'>
-        <h4>1</h4>
-      </Table>
-      <Table width = '50px'>
-        <h4>2</h4>
-      </Table>
-      <Table width = '50px'>
-        <h4>3</h4>
-      </Table>
-      <Table width = '50px'>
-        <h4>4</h4>
-      </Table>
-      <Table width = '50px'>
-        <h4>5</h4>
-      </Table>
-      <Table width = '50px'>
-        <h4>6</h4>
-      </Table>
-      <Table width = '50px'>
-        <h4>7</h4>
-      </Table>
-      <Table width = '50px'>
-        <h4>8</h4>
-      </Table>
-      <Table width = '50px'>
-        <h4>9</h4>
-      </Table>
-      <Table width = '50px'>
-        <h4>10</h4>
-      </Table>
-      <Table width = '50px'>
-        <h4>11</h4>
-      </Table>
-      <Table width = '50px'>
-        <h4>12</h4>
-      </Table>
-      <Table width = '50px'>
-        <h4>13</h4>
-      </Table>
-      <Table width = '50px'>
-        <h4>14</h4>
-      </Table>
-      <Table width = '50px'>
-        <h4>15</h4>
-      </Table>
-      <Table width = '50px'>
-        <h4>16</h4>
-      </Table>
-      <Table width = '50px'>
-        <h4>17</h4>
-      </Table>
-      <Table width = '50px'>
-        <h4>18</h4>
-      </Table>
-      <Table width = '50px'>
-        <h4>19</h4>
-      </Table>
-      <Table width = '50px'>
-        <h4>20</h4>
-      </Table>
-      <Table width = '50px'>
-        <h4>21</h4>
-      </Table>
-      <Table width = '50px'>
-        <h4>22</h4>
-      </Table>
-      <Table width = '50px'>
-        <h4>23</h4>
-      </Table>
-      <Table width = '50px'>
-        <h4>24</h4>
-      </Table>
+        <h4 style={{width: '100px'}}>Hours</h4>
+        <h4 style={{width: '50px'}}>7</h4>
+        <h4 style={{width: '50px'}}>8</h4>
+        <h4 style={{width: '50px'}}>9</h4>
+        <h4 style={{width: '50px'}}>10</h4>
+        <h4 style={{width: '50px'}}>11</h4>
+        <h4 style={{width: '50px'}}>12</h4>
+        <h4 style={{width: '50px'}}>13</h4>
+        <h4 style={{width: '50px'}}>14</h4>
+        <h4 style={{width: '50px'}}>15</h4>
+        <h4 style={{width: '50px'}}>16</h4>
+        <h4 >17</h4>
     </Table>
     {/* each Table past here is a new row. Right now these are just the titles */}
     <Table  container flexDirection = "row">
-      <h4>Room 1</h4>
+      <h4 style={{width: '100px'}}>Room 1</h4>
+      <button style={BUTTON}></button>
+      <button style={BUTTON}></button>
     </Table>
     <Table container flexDirection = "row">
       <h4>Room 2</h4>
     </Table>
   </Table>
 )
+
+const BUTTON ={
+  width: '50px',
+  height: '50px',
+  backgroundColor: '#1f97e5',
+}
 
 export default App;
